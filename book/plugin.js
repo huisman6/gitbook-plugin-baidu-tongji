4fdb083f240becf2c4159c9f523880d9dc565dfc
戚雨 <yuqi@wisedu.com>
2017-04-10 13:54:59 +0800

    配置文件名称调整

--- book/plugin.js
+++ book/plugin.js
@@ -1,8 +1,8 @@
 require(["gitbook"], function(gitbook) {
     gitbook.events.bind("start", function(e, config) {
-        config.baidu = config.baidu || {};
+        config["baidu-wis"] = config["baidu-wis"] || {};
         var hm = document.createElement('script');
-        hm.src = '//hm.baidu.com/hm.js?' + config.baidu.token;
+        hm.src = '//hm.baidu.com/hm.js?' + config["baidu-wis"].token;
         var s = document.getElementsByTagName('script')[0];
         s.parentNode.insertBefore(hm, s);
     });
