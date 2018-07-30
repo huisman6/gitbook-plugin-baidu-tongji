ef1601f395b8dc576377476225cd4336939081e0
huisman <huisman@dooioo.com>
2018-07-30 17:04:30 +0800

    百度统计url走配置。

--- book/plugin.js
+++ book/plugin.js
@@ -1,9 +1,8 @@
 require(["gitbook"], function(gitbook) {
     gitbook.events.bind("start", function(e, config) {
         config["baidu-tongji"] = config["baidu-tongji"] || {};
-        var url=config["baidu-tongji"].url || "https://hm.baidu.com/hm.js";
         var hm = document.createElement('script');
-        hm.src =url+'?' + config["baidu-tongji"].token;
+        hm.src =config["baidu-tongji"].url+'?' + config["baidu-tongji"].token;
         var s = document.getElementsByTagName('script')[0];
         s.parentNode.insertBefore(hm, s);
     });
